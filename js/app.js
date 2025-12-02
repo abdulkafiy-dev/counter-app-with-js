ce05338cdddfa872917a099d0918adbd6ec6fa0b
Abdulkafiy <abdulkafiy20@outlook.com>
2025-12-02 08:02:18 +0100

    Maximum and Minimum limits was added to the counter

--- js/app.js
+++ js/app.js
@@ -21,23 +21,40 @@ const resetBtn = document.querySelector('#reset');
 // Count state init count
 let count = 0
 
+// Store Minimum &amp; Maximum Limits for counting
+const MIN_LIMIT = 0;
+const MAX_LIMIT = 25;
 
 const updateDisplayCount = () => {
+    /** 
+     * if limit MAX_LIMIT, counter color changes to red
+     * &amp; if reaches MIN_LIMIT it changes to yellow
+    */
+    if(count >= MAX_LIMIT){
+        displayCount.style.color = "red";
+    } else if(count <= MIN_LIMIT){
+        displayCount.style.color = "yellow";
+    } else{
+        displayCount.style.color = "white";
+    }
     displayCount.textContent = count;
 }; // Display Count Func
 
 const setDecreaseCount = () => {
+    if(count > MIN_LIMIT)
     count --;
     updateDisplayCount();
 }; // Decrease Count Func
 
 const setIncreaseCount = () => {
+    if(count < MAX_LIMIT)
     count ++;
     updateDisplayCount();
 }; // Increase Count Func 
 
 const setResetCount = () => {
     count = 0;
+    count = MIN_LIMIT;
     updateDisplayCount();
 }; // Reset / Clear Recently Counted Num
 
